f2c33442ec2f7229a9fee845a1467c9a7eaa8332
sZagal04 <szagal04@gmail.com>
2026-05-16 15:23:50 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,13 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Ariana",
+    "phone": "+10005553535",
+    "address": "123 Elm Street, Hilltop"
+}
+
+kit_body = {
+    "name": "a"
+}
